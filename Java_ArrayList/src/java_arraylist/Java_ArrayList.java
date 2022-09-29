65bbf562fb0bf73d7f79f279f9c41b7555bda27b
FarukKalil <faruk1.kalil@gmail.com>
2022-09-29 20:16:02 -0300

    ArrayList

---
+++ Java_ArrayList/src/java_arraylist/Java_ArrayList.java
@@ -0,0 +1,77 @@
+package java_arraylist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Java_ArrayList {
+
+    public static void main(String[] args) {
+        System.out.println("---------- ARRAYLIST ----------");
+        //addDadosArrayList();
+        //buscaNomeListaPorPosicao();
+        //limpaListaClear();
+        //limpaListaRemoveAll();
+        //removeDadoLista();
+        //atualizaDadoLista();
+    }
+    /*Adicionando dados em uma ArrayList */
+    public static void addDadosArrayList(){
+        // instanciando classe Arraylist
+        ArrayList<String> listaNome = new ArrayList<>();
+        listaNome.add("EducaCiencia FastCode");
+        listaNome.add("CobraDev");
+        System.out.println("Lista: " + Arrays.toString(listaNome.toArray()));
+    }
+    
+    /* Buscando Nome na lista por posição */
+    public static void buscaNomeListaPorPosicao() {
+        ArrayList<String> listaNome = new ArrayList<>();
+        listaNome.add("EducaCiencia FastCode");
+        listaNome.add("CobraDev");
+        System.out.println("Buscando nome da posição 1 " +  listaNome.get(1)); // buscando posição 1 = CobraDev
+        System.out.println("Buscando nome da posição 0 " + listaNome.get(0));  // buscando posição 0 = Educa
+    }
+    
+    /* limpando lista utilizando Clear */
+    public static  void limpaListaClear() {
+        ArrayList<String> listaNome = new ArrayList<>();
+        listaNome.add("EducaCiencia FastCode");
+        listaNome.add("CobraDev");
+        System.out.println("Lista: " + Arrays.toString(listaNome.toArray()));
+        System.out.println(" ***** removendo dados da lista *****");
+        listaNome.clear();
+        System.out.println("Lista Atualizada: " + Arrays.toString(listaNome.toArray()));
+    }
+    
+    /* limpando lista utilizando RemoveAll */
+    public static void limpaListaRemoveAll(){
+        ArrayList<String> listaNome = new ArrayList<>();
+        listaNome.add("EducaCiencia FastCode");
+        listaNome.add("CobraDev");
+        System.out.println("Lista: " + Arrays.toString(listaNome.toArray()));
+        System.out.println(" ***** removendo dados da lista *****");
+        listaNome.removeAll(listaNome); // removendo dados da lista
+        System.out.println("Lista Atualizada: " + Arrays.toString(listaNome.toArray()));
+    }
+    
+    /*excluindo dado da lista utilizando Remove*/
+    public static void removeDadoLista() {
+        ArrayList<String> listaNome = new ArrayList<>();
+        listaNome.add("EducaCiencia FastCode");
+        listaNome.add("CobraDev");
+        System.out.println("Lista: " + Arrays.toString(listaNome.toArray()));
+        System.out.println(" ***** removendo dados da lista *****");
+        listaNome.remove("CobraDev"); // removendo CobraDev
+        System.out.println("Lista Atualizada: " + Arrays.toString(listaNome.toArray()));
+    }
+    
+    public static void atualizaDadoLista() {
+        ArrayList<String> listaNome = new ArrayList<>();
+        listaNome.add("EducaCiencia FastCode");
+        listaNome.add("CobraDev");
+        System.out.println("Lista: " + Arrays.toString(listaNome.toArray()));
+        System.out.println(" ***** atualizando dado na lista *****");
+        listaNome.set(listaNome.indexOf("EducaCiencia FastCode"), "EDUCACIENCIA FASTCODE"); // atualizado nome EducaCiencia fastcode para EDUCACIENCIA FASTCODE
+        System.out.println("Lista Atualizada: " + Arrays.toString(listaNome.toArray()));
+     }
+}
